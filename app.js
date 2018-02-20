ad2a0b8233798e517f3c5c24132b2fcf7a773021
Jesse Atay <jesse.atay@gmail.com>
2018-02-20 12:03:01 -0800

    updated shark

---
+++ app.js
@@ -0,0 +1,13 @@
+var form = document.getElementById('form');
+var cost = document.getElementById('cost');
+var shark = 3960;
+var submitButton = document.getElementById('submit');
+
+form.addEventListener('submit' , (event) => {
+    event.preventDefault();
+   var x = parseInt(form.elements[1].value);
+   var answer = (x/shark);
+
+   cost.innerHTML = `Your item costs ${answer} sharks.`;
+
+});
