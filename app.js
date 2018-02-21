476d50b4d81b00e61ec14821f482deed5e4f2a7d
Jesse Atay <jesse.atay@gmail.com>
2018-02-20 18:57:35 -0800

    finished

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var shark = 3960;
 var submitButton = document.getElementById('submit');
 
 form.addEventListener('submit' , (event) => {
+    cost.innerHTML = '';
     event.preventDefault();
    var x = parseInt(form.elements[1].value);
    var answer = (x/shark);
